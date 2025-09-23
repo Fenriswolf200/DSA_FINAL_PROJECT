d50fcbcc61529bf5b7c93677e4b06d26f068c5e0
Fenriswolf200 <guri020406@gmail.com>
2025-09-23 02:14:18 +0200

    Added functions to check validity of movements

--- game.py
+++ game.py
@@ -8,6 +8,19 @@ class MarbleGame:
         self.board = self.create_board(size)
         self.size = size
         
+        self.movement_vectors = {
+            "up": np.array([-2,0]),
+            "down": np.array([2,0]),
+            "left": np.array([0,-2]),
+            "right": np.array([0,2])
+        }
+        
+        self.midpoint_movement_vectors = {
+            "up": np.array([-1,0]),
+            "down": np.array([1,0]),
+            "left": np.array([0,-1]),
+            "right": np.array([0,1])
+        }
 
     def create_board(self, size) -> np.array:
         board = np.full((size, size), 1)
@@ -17,16 +30,39 @@ class MarbleGame:
             for col in range(size):
                 if (row < size // 3 or row >= size - size // 3) and (col < size // 3 or col >= size - size // 3):
                     board[row, col] = 2
+
         return board
 
+    def check_movement_validity(self, selected_marble: np.array, new_position: np.array, midpoint: np.array):
         
-    
-    def check_movement(self, selected_marble:np.array, destination:np.array) -> bool:
+        for pos in (selected_marble, new_position, midpoint):
+            if pos[0] < 0 or pos[0] >= self.size or pos[1] < 0 or pos[1] >= self.size:
+                return False
+
+        if self.board[selected_marble[0], selected_marble[1]] != 1:
+            return False
+        if self.board[midpoint[0], midpoint[1]] != 1:
+            return False
+        if self.board[new_position[0], new_position[1]] != 0:
+            return False
+
         return True
 
 
-    def move_marble(self, selected_marble:np.array, direction:str)-> None:
-        pass
+    def move_marble(self, selected_marble:np.array, direction:str)-> bool:
+        
+        new_position = np.sum(selected_marble, self.movement_vectors[direction])
+        midpoint = np.sum(selected_marble, self.midpoint_movement_vectors[direction])
+        
+        if self.check_movement_validity(selected_marble, new_position, midpoint) == False:
+            return False
+        
+        
+        self.board[new_position[0]][new_position[1]]=1
+        self.board[selected_marble[0]][selected_marble[1]]=0
+        self.board[midpoint[0]][midpoint[1]]=0
+        
+        return True
 
 
     def render(self):
