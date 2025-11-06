1e6476ff2001b539eb3804831f337da342050d3f
Icíar <iciaradelinoordax@gmail.com>
2025-11-06 11:03:00 +0100

    imporved models + linked list for hands

--- models.py
+++ models.py
@@ -8,37 +8,76 @@ class Card:
     
     def __repr__(self): #to display the card in the console like "A♠"
         return f"{self.rank}{self.suit}"
+    
+    def __eq__(self, other): #called when we use == to compare two cards
+        if isinstance(other, Card):
+            return self.suit == other.suit and self.rank == other.rank
+        return False
 
 
 class Deck:
     def __init__(self):
         suits = ['♠', '♥', '♦', '♣']
         ranks = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
-        self.cards = [Card(suit, rank) for suit in suits for rank in ranks]
+        self.cards = [Card(suit, rank) for suit in suits for rank in ranks] # uses an array to store the cards
         self.shuffle()
     
-    def shuffle(self):
+    def shuffle(self): # maybe we can change this for our own shuffle algorithm
         random.shuffle(self.cards)
+    
+    def draw(self):
+        if self.cards:
+            return self.cards.pop()
+        return None
     
     def deal(self, num_cards: int):
         return [self.cards.pop() for _ in range(num_cards)]
 
 
-class Hand:
+class Node:
+    def __init__(self, card: Card):
+        self.card = card
+        self.next = None
+
+
+class Hand: #uses a linked list to store the cards in the hand
     def __init__(self):
-        self.cards = []
+        self.head = None
     
     def add_card(self, card: Card):
-        self.cards.append(card)
+        new_node = Node(card)
+        if self.head is None:
+            self.head = new_node
+        else:
+            current = self.head
+            while current.next:
+                current = current.next
+            current.next = new_node
     
     def remove_card(self, card: Card):
-        if card in self.cards:
-            self.cards.remove(card)
+        if self.head is None:
+            return False
+        
+        if self.head.card == card:
+            self.head = self.head.next
             return True
+        
+        current = self.head
+        while current.next:
+            if current.next.card == card:
+                current.next = current.next.next
+                return True
+            current = current.next
+        
         return False
     
-    def __repr__(self): #display hand as a string like "[A♠, 2♥, 3♦, 4♣]"
-        return str(self.cards)
+    def __repr__(self):
+        cards = []
+        current = self.head
+        while current:
+            cards.append(str(current.card))
+            current = current.next
+        return str(cards)
 
 
 class Player:
